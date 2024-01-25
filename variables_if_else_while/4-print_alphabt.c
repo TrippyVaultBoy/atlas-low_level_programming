67e444d7ce59d42247b5ae5b36194e4f619b8ef7
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 04:42:40 +0000

    added 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == e)
+		{
+
+		}
+		else if (a == q)
+		{
+
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+	return (0);
+}
