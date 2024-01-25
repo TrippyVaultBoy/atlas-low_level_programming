0e7dbc509250e88ca9504702839d34a88a2153d6
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 04:04:50 +0000

    added 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(int)
+{
+	for(char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(%c, c);
+	}
+	return(0);
+}
