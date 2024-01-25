057ca55fceccfa653d643012dce46b2fb59fdb64
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 06:16:14 +0000

    edit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,13 +12,7 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		putchar(i + 0);
-
-		for (j = 48; i <= 57; i++)
-		{
-			putchar(j + 0);
-			putchar(',');
-		}
+		putchar(i + '0');
 	}
 	return (0);
 }
