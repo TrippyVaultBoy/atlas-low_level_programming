1b29276a8fd7a9ff9e12198502c304d0ebc592f6
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 04:50:06 +0000

    added 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num < 10; num++)
+	{	
+		printf(num);
+	}
+	return (0);
+}
