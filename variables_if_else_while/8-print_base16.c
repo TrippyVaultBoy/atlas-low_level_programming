d07f19fa8a7219c1b0debf3194915f0d00936a93
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 05:50:24 +0000

    added 8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 0);
+	}
+
+	for (x == a; x <= f; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
