b6991c29983d26da61bac312f2be74b1b0b3c59a
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 04:34:16 +0000

    added 3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int  main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+}
