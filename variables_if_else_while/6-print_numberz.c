91ecabf183e31830b220d3d4e565c2e9b0771642
Tanner Saint <tsaint2004@gmail.com>
2024-01-25 05:27:44 +0000

    edit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar('num');
+	}
+	putchar('\n');
+	return (0);
+}
