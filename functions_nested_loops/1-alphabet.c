0400da472b4c023258f31646fa12eda461d86b74
Tanner Saint <tsaint2004@gmail.com>
2024-01-27 00:11:35 +0000

    edit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main- calls print_alphabet()
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
