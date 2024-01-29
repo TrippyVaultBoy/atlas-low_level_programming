0608059baa9be043709047a2a27ff6d59edfa27a
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 16:52:52 +0000

    added 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line- prints a straight line
+ * @n: length of line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+}
