695e506d481cfee6738102faa4e5d567ce2770c3
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 08:57:22 +0000

    edit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 	int count;
 	char num;
 
-	for (count = 1; count <= 10; count++)
+	for (count = 1; count < 10; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num >= 10)
+			if (num > 10)
 			{
 				_putchar((num / 10) + '0');
 				_putchar((num % 10) + '0');
