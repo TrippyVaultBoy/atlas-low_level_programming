c30b49f2181e83cd43185a0b8734a857fa144ad4
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 19:14:01 +0000

    edit

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,21 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(' ');
-		
-		if (n > 0)
+		for (j = 0; j < n; j++)
 		{
-			_putchar('\\');
-			_putchar('\n');
+			if (i == j)
+			{
+				_putchar('\\');
+			}
+			else
+			{
+				_putchar(' ');
+			}
 		}
-		else
-		{
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
