63a80bdff2b5f0274430ffae5b184cfcdcf11aa1
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 06:07:45 +0000

    added 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#insert "main"
+
+/**
+ * print_last_digit- prints the last digit of a number
+ * @n: integer entered
+ *
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
