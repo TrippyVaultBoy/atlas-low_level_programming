4fcbc3bdc7d0b5b32c2e9a5a624c78a54d353c12
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 04:46:29 +0000

    added 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower- checks for a lowercase character
+ *
+ * Return: 1 if lowercase 0 if not
+ */
+
+int _islower(int c);
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
