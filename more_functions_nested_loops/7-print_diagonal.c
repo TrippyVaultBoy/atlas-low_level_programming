18d845ff50e3c7f27fab65984795a399148f4082
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 16:58:43 +0000

    added 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal- prints a diagonal line
+ * @n: length of line
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\');
+	_putchar('\n');
+}
