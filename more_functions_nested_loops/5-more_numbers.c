c5e21066ac73bc45b280c231352d1fb434fe62f5
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 08:55:08 +0000

    added 4

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints 0-14 10 times
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int count;
+	char num;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + '0');
+				-putchar((num % 10) + '0');
+			}		
+		}
+		_putchar('\n');
+	}
+}
