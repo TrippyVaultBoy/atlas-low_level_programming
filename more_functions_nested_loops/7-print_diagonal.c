735ae4f7344c8e8386d1b31519fb2f5aa92b05a9
Tanner Saint <tsaint2004@gmail.com>
2024-01-29 19:24:36 +0000

    edit

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * print_diagonal- prints a diagonal line
- * @n: length of line
- *
- * Return: void
- */
+* print_diagonal - draws a diagonal line
+* @n: lenngth of line
+*
+* Return: void
+*/
 
 void print_diagonal(int n)
 {
 	int i;
 	int j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= i; j++)
 		{
 			if (i == j)
 			{
 				_putchar('\\');
+				_putchar('\n');
 			}
 			else
 			{
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
 	}
 }
