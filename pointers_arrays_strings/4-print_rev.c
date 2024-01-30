3fd53a20511de5c50b5af07db7cf4d200e5204cb
Tanner Saint <tsaint2004@gmail.com>
2024-01-30 04:26:44 +0000

    edit

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@
 void print_rev(char *s)
 {
 	int len;
-        len = _strlen(*s);
+        len = _strlen(s);
 
-	*s = *s + len;
+	s = s + len - 1;
 
-	while (*s != '\n')
+	while (s != '\0')
 	{
-		_putchar(*s);
-		*s--;
+		_putchar(s);
+		s--;
 	}
 	_putchar('\n');
 }
