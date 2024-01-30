b62e1dd4114e75d21e6f5b2cc823eef729c2938d
Tanner Saint <tsaint2004@gmail.com>
2024-01-30 04:17:08 +0000

    added 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev- prints a string in reverse
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(*s);
+
+	*s = *s + len
+
+	while (*s != '\n')
+	{
+		_putchar(*s);
+		*s--;
+	}
+	_putchar('\n');
+}
