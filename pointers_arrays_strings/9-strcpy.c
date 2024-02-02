27d5eb557a30478e704408823f51f3fafa34881e
Tanner Saint <tsaint2004@gmail.com>
2024-02-02 02:21:02 +0000

    edit

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strcpy- copies a string
+ * @dest: buffer to coppy string to
+ * @src: input string
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	
+}
