51ace6b7ba86c4563a5387e137e43af61c3e6d5f
Tanner Saint <tsaint2004@gmail.com>
2024-02-02 23:43:58 +0000

    edit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,5 +12,25 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int destlen;
+	int srclen;
+	int charcount;
+
+	charcount = 0;
+
+	for (destlen = 0; dest[destlen] != '\0'; destlen++)
+	{
+	}
+
+	for (srclen = 0; src[srclen] != '\0'; srclen++)
+	{	
+		if (charcount < n)
+		{
+			dest[destlen] = src[srclen];
+			destlen++;
+			charcount++;
+		}
+	}
+
+	return (dest);	
 }
