80a35409ea6022b2d7dfb67c1fcec5d1b03137f3
Tanner Saint <tsaint2004@gmail.com>
2024-02-02 06:31:45 +0000

    edit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,22 +10,45 @@
 int _atoi(char *s)
 {
 	int i;
+	int j;
+	int n;
 	int pos;
 	int neg;
+	int firstdigit;
+	int secdigit;
 
 	pos = 0;
 	neg = 0;
 
-	for (i = 0; s[i] >= '0' && s[i] <= '9'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '+')
 		{
 			pos++;
 		}
-		else if (s[i] == '-')
+		if (s[i] == '-')
 		{
 			neg++;
 		}
 	}
-	return (neg);
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			firstdigit = j;
+			break;
+		}
+	}
+
+	for (n = j; s[n] != '\0'; n++)
+	{
+		if (s[n] <= '0' && s[n] >= '9')
+		{
+			secdigit = n;
+			break;
+		}
+	}
+
+	return (firstdigit);
 }
