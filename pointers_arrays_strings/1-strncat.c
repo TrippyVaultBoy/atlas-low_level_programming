35350a6d55beb173f6da7a6dee994f87b6e745c2
Tanner Saint <tsaint2004@gmail.com>
2024-02-02 23:06:37 +0000

    edit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strncat- concatenates two strings 
+ * and uses at most n bytes from src
+ * @dest: concatinated string
+ * @src: string to be added
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	
+}
