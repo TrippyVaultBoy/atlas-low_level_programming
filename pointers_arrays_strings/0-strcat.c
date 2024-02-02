41d28b8dd783876bd452b5b6cf23a891f11b0124
Tanner Saint <tsaint2004@gmail.com>
2024-02-02 22:59:56 +0000

    edit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * overwriting the terminating null byte (\0) at the
  * end of dest, and then adds a terminating null byte
  * @dest: string to be appended to
- * @scr: string to be appended
+ * @src: string to be appended
  *
  * Return: pointer to the string dest
  */
