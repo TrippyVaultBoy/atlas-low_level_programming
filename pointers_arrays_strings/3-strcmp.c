a7a215c9f8a5e4bc47fccb58ef1e51ebb6460c41
Tanner Saint <tsaint2004@gmail.com>
2024-02-03 22:11:00 +0000

    edit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int result;
-
-	result = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		result += (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
 
-	return (result);
+	return (0);
 }
