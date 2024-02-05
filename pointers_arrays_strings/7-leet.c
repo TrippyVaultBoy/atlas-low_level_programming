6fa4dd7dd39a06b7deefbf27a186f353ca5dfc75
Tanner Saint <tsaint2004@gmail.com>
2024-02-05 05:49:40 +0000

    edit

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *leet- encodes a string into leet 
+ * @str: input string
+ *
+ * Return: pointer to string
+ */
+
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ()
+		{
+
+		}
+	}
+}
