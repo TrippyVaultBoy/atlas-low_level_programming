7588fb28c8e8a3315bca862afbd05926eb37006b
tsaint2004@gmail.com <Tanner>
2024-02-09 01:59:40 +0000

    edit

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int prime_num(int num, int count)
+{
+	if (num % count == 0)
+	{
+		return (0);
+	}
+	else if (count >= num / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (prime_num(num, count + 1));
+	}
+}
+
+/**
+ * is_prime_number- checks is a number is a prime number recursively
+ * @n: number to be checked
+ *
+ * Return: 1 is yes, 0 if no
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 0 || n == 1 || n < 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime_num(n, 2));
+	}
+}
--- recursion/main.h
+++ recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square_root(int val, int counter);
+int prime_num(int num, int count);
+int is_prime_number(int n);
 
 #endif
