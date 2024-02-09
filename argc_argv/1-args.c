330e781fcefb241b2318f6c92f79c110b2933a47
tsaint2004@gmail.com <Tanner>
2024-02-09 22:35:44 +0000

    edit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_args- prints the number of arguments passed into main
+ * @num_args- number of args 
+ *
+ * Return: void
+ */
+
+void print_args(int num_args)
+{
+
+}
+
+/**
+ * main- main function
+ * @argc- number of arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
