43fab306508f8db651ff3d995f289419ea49c0fc
tsaint2004@gmail.com <Tanner>
2024-02-13 04:51:39 +0000

    edit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main- adds positive numbers
+ * @argc: number of arguments
+ * @argv: arguments listed
+ *
+ * Return: if a non digit character is entered return 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 1; (i < argc) != 0; i++)
+	{
+		if (isdigit(atoi(argv[i])) != 0)
+		{
+			printf("Error");
+			return (1);
+		}
+
+		if (atoi(argv[i]) > 0)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	
+	printf("%d\n", sum);
+	return (0);
+
+}
