fd94ce952b5461846bf4c81325f638e4d96d11b6
tsaint2004@gmail.com <Tanner>
2024-02-16 23:19:45 +0000

    edit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -21,17 +21,15 @@ char *create_array(unsigned int size, char c)
 
 	array = (char*)malloc(size * 4);
 
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 
-	if (*array == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		return (array);
-	}
+	return (array);
 }
