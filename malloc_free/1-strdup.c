eb9b047a950c3affbccf30e85278e1de6e060670
tsaint2004@gmail.com <Tanner>
2024-02-17 19:56:26 +0000

    edit

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,16 +14,16 @@ char *_strdup(char *str)
 	int j;
 	char *array;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
 
 	array = (char *)malloc(i + 1);
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
