9bce6dc74d5528b357bb01e1fc076ac785b857c9
tsaint2004@gmail.com <Tanner>
2024-02-25 23:33:30 +0000

    edit

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * struct init_ dog- initialises data of dog
+ * struct init_dog- initialises data of dog
  * @name: name of the dog
  * @age of the dog
  * @owner: owner of the dog
