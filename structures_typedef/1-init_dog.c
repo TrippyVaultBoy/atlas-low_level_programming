922d3dc5d85e0389972cce5f399807001d374921
tsaint2004@gmail.com <Tanner>
2024-02-25 23:28:48 +0000

    edit

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,36 +1,10 @@
 #include "dog.h"
 #include <stdlib.h>
-
-/**
- * *_strncpy- copies a string
- * @dest: string to be copied to
- * @src: string to be copied
- * @n: number of bytes to be copied
- *
- * Return: pointer to dest
- */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-
-	return (dest);
-}
-
-
+#include <stdio.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	*_strncpy	
+{	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: owner of dog
  */
 
-struct dog
+typedef struct dog
 {
-	char *name[50];
+	char *name;
 	float age;
-	char *owner[50];
-};
+	char *owner;
+} dog_t;
 
 char *_strncpy(char *dest, char *src, int n);
 void init_dog(struct dog *d, char *name, float age, char *owner);
