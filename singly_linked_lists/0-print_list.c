7dbec382b738cf93b5eed61e6c2fc25627e9b2e7
tsaint2004@gmail.com <Tanner>
2024-03-09 18:36:49 +0000

    edit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+ struct Node 
+	{
+		int data;
+		struct Node* next;
+        }list_t;
+
 size_t print_list(const list_t *h)
 {
-
+	struct Node* current = head;
 }
