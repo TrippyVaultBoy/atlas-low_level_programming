186f57c41b23f1b7fa0717168e8627ccd0753c78
tsaint2004@gmail.com <Tanner>
2024-03-10 18:51:37 +0000

    edit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		} else {
 			count++;
-			printf("[%d] %s", count, h->str);
+			printf("[%d] %s", h->len, h->str);
 		}
 		h = h->next;
 	}
