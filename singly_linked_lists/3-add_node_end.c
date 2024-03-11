1739520b64bc1b9d586681978a3aeb3bcb60691e
tsaint2004@gmail.com <Tanner>
2024-03-11 04:23:22 +0000

    edit

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,5 +13,43 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *now;
+	list_t *newNode;
+	int len;
 
+	now = *head;
+
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+
+	str = str - len;
+
+	while(now != NULL && now->next != NULL)
+	{
+		now = now->next;
+	}
+
+	newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+
+	if (newNode->str = NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->len = len;
+	newNode->next = NULL;
+
+	return (newNode);
 }
