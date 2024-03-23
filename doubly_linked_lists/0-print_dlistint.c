6a77498f7f57889baafe6235243e548c5e281f0c
Tanner Saint <tsaint2004@gmail.com>
2024-03-23 00:02:17 +0000

    added 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint- prints a doubly linked list
+ * @h: pointer to head of the list
+ * 
+ * Return: the number of nodes in the list
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+}
---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
