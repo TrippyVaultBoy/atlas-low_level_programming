44fae4f79ec3822de8747cee472646177a5a9256
Tanner Saint <tsaint2004@gmail.com>
2024-03-25 01:32:31 +0000

    added 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint- add a node to the beginning of a list
+ * @head: head of the list
+ * @n: node data
+ * 
+ * Return address of the new element or NULL
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+}
