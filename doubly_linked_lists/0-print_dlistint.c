43d28b0c38e18e320d0c59d65481be1c8fb9bf6a
Tanner Saint <tsaint2004@gmail.com>
2024-03-25 01:06:08 +0000

    edit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -21,7 +21,7 @@ size_t print_dlistint(const dlistint_t *h)
         temp = temp->prev;
     }
 
-    while (temp->next != NULL)
+    while (temp != NULL)
     {
         printf("%d", temp->n);
         count++;
