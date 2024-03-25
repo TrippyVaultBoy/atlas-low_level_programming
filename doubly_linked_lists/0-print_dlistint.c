f4ad3a21544609fd27a8e038216fb5d07cfdc013
Tanner Saint <tsaint2004@gmail.com>
2024-03-25 01:15:00 +0000

    edit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -16,6 +16,11 @@ size_t print_dlistint(const dlistint_t *h)
     int count;
     count = 0;
 
+    if (h == NULL)
+    {
+        return (count);
+    }
+
     while (temp->prev != NULL)
     {
         temp = temp->prev;
