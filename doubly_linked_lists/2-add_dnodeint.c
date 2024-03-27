dfdcdab6b21064b42b877ae26446ca3594428966
Tanner Saint <tsaint2004@gmail.com>
2024-03-27 00:26:27 +0000

    edit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,10 +20,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
+
 	new_node->prev = NULL;
 	new_node->next = *head;
 
 	*head = new_node;
 
-	return (new_node);
+	return (&new_node);
 }
