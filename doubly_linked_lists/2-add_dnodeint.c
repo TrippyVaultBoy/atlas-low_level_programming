39c4cd21ce244f275e0e545050b6cba7b3fa45c9
Tanner Saint <tsaint2004@gmail.com>
2024-03-27 00:35:10 +0000

    made betty compliant

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -7,13 +7,14 @@
  * add_dnodeint- add a node to the beginning of a list
  * @head: head of the list
  * @n: node data
- * 
+ *
  * Return address of the new element or NULL
 */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
