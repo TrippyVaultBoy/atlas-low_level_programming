4df588aa341c1dc30e2f6f5b607639d2986119b3
Tanner Saint <tsaint2004@gmail.com>
2024-03-27 00:22:06 +0000

    added return statement

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = head;
+	new_node->next = *head;
 
 	*head = new_node;
+
+	return (new_node);
 }
