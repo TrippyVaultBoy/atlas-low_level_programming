b1777e0a916fd5454c059847abacf4adf9c7823d
Tanner Saint <tsaint2004@gmail.com>
2024-03-28 08:20:43 +0000

    removed unecisary code

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -15,11 +15,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 		count = 0;
 
-		if (head->prev != NULL)
-		{
-			head = head->prev;
-		}
-
 		while (head->next != NULL)
 		{
 			if (count == index)
