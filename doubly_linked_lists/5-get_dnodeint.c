93606198923c79ba7eb2e81e494792031a97486d
Tanner Saint <tsaint2004@gmail.com>
2024-03-28 07:59:20 +0000

    added 5-get_dlistint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head != NULL)
+	{
+		dlistint_t *current_node = head;
+		unsigned int count;
+
+		count = 0;
+
+		while (current_node->next != NULL)
+		{
+			if (count == index)
+			{
+				return (current_node);
+			}
+			current_node = current_node->next;
+			count++;
+		}
+	}
+
+	return (NULL);
+}
