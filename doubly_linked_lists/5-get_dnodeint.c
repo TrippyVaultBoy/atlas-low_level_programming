43c1ff72e37deddf276acf080c6d8f28d3408376
Tanner Saint <tsaint2004@gmail.com>
2024-03-28 08:14:44 +0000

    removed unnecisary pointer

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	if (head != NULL)
 	{
-		dlistint_t *current_node = head;
 		unsigned int count;
 
 		count = 0;
@@ -21,13 +20,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 			head = head->prev;
 		}
 
-		while (current_node->next != NULL)
+		while (head->next != NULL)
 		{
 			if (count == index)
 			{
-				return (current_node);
+				return (head);
 			}
-			current_node = current_node->next;
+			head = head->next;
 			count++;
 		}
 	}
