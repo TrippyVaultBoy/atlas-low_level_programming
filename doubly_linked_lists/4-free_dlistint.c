0d767b140c81943fe8d257a335ae0943e44bec6c
Tanner Saint <tsaint2004@gmail.com>
2024-03-28 07:47:47 +0000

    made betty compliant

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -5,21 +5,21 @@
 
 /**
  * freedlistint- frees a list
- * @head: pointer to head of the list 
+ * @head: pointer to head of the list
 */
 
 void free_dlistint(dlistint_t *head)
 {
-    if (head != NULL)
-    {
+	if (head != NULL)
+	{
 		dlistint_t *current_node = head;
 
 		while (current_node->next != NULL)
 		{
 			current_node = current_node->next;
-			free (current_node->prev);
+			free(current_node->prev);
 		}
 
-		free (current_node);
-    }
+		free(current_node);
+	}
 }
