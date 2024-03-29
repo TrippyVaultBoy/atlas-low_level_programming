ca7776362f523a18dd05db0820000fd99640d19e
Tanner Saint <tsaint2004@gmail.com>
2024-03-29 03:26:29 +0000

    added code 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current_node = *h;
+	unsigned int i;
+	new_node->n = n;
+
+	i = 0;
+
+	if (*h != NULL)
+	{
+		if (idx == 0)
+		{
+			new_node->next = *h;
+			(*h)->prev = new_node;
+			*h = new_node;
+			return (new_node);
+		}
+
+		while (current_node != NULL && i < idx)
+		{
+			current_node = current_node->next;
+			i++;
+		}
+
+		new_node->next = current_node;
+		new_node->prev = current_node->prev;
+
+		current_node->prev = new_node;
+
+		return (new_node);
+	}
+	else
+	{
+		*h = new_node;
+		return (new_node);
+	}
+}
