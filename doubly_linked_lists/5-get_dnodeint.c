03f72120ac9456d912d5a2d6aa2ef3a520919713
Tanner Saint <tsaint2004@gmail.com>
2024-04-01 01:28:45 +0000

    added kernal documentation

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,11 @@
 #include "lists.h"
 
 /**
+ * get_dlistint_at_index- get the value of a node at a certain index
+ * @head: pointer to the head of the list
+ * @index: index of node to be returned
  *
+ * Return: value at index
 */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
