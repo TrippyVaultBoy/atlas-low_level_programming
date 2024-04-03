375c790ca5aac4f2a10a5bea952818738c88326c
Tanner Saint <tsaint2004@gmail.com>
2024-04-03 03:51:42 +0000

    added length declaration

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current_node = *h;
 
 	unsigned int counter;
+	unsigned int length;
 	new_node->n = n;
 	counter = 0;
+	length = 0;
 
 	while (current_node->next != NULL)
 	{
