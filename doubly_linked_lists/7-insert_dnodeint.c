a7882a4c3293f08e4691fe8c5e45614c62e7506b
Tanner Saint <tsaint2004@gmail.com>
2024-04-03 03:03:49 +0000

    changed while loop

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	counter = 0;
 
-	while (counter < idx && current_node->next != NULL)
+	while (counter < idx && current_node != NULL)
 	{
 		current_node = current_node->next;
 		counter++;
