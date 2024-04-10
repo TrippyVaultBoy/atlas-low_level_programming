ef4f0922a4703a9594c16cfce3f6436681f98f01
Tanner Saint <tsaint2004@gmail.com>
2024-04-10 23:13:30 +0000

    made betty compliant

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -6,45 +6,44 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    int middle;
-    int i;
-    int min;
-    int max;
+	int middle;
+	int i;
+	int min;
+	int max;
 
-    min = 0;
-    max = size - 1;
+	min = 0;
+	max = size - 1;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    while (min <= max)
-    {
-        middle = ((min + max) / 2);
+	while (min <= max)
+	{
+		middle = ((min + max) / 2);
 
-        printf("Searching in array: ");
+		printf("Searching in array: ");
 
-        for (i = min; i < max; i++) {
-            printf("%d, ", array[i]);
-        }
-        printf("%d", array[i]);
-        printf("\n");
+		for (i = min; i < max; i++) {
+			printf("%d, ", array[i]);
+		}
+		printf("%d", array[i]);
+		printf("\n");
 
-        if (value == array[middle])
-        {
-            return (array[middle]);
-        }
+		if (value == array[middle])
+		{
+			return (array[middle]);
+		}
 
-        if (value < middle)
-        {
-            max = middle - 1;
-        }
-        else
-        {
-            min = middle + 1;
-        }
-    }
-
-    return (-1);
+		if (value < middle)
+		{
+			max = middle - 1;
+		}
+		else
+		{
+			min = middle + 1;
+		}
+	}
+	return (-1);
 }
