b31e0399187257b3fdc1dcf130eb140c57eedeba
Tanner Saint <tsaint2004@gmail.com>
2024-04-10 22:12:41 +0000

    implemented linear_search()

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,5 +6,20 @@
 
 int linear_search(int *array, size_t size, int value)
 {
+    for (i = 0; i < size - 1; i++)
+    {
+        if (array == NULL)
+        {
+            return (-1);
+        }
 
+        printf("Value checked array[%d]\n", array[i]);
+        if (array[i] == value)
+        {
+            printf("Found %d at index: %d\n", array[i], i);
+            return (array[i]);
+        }
+
+        return (-1);
+    }
 }
