deb4d7b1455ac8ac89bb770c69e154553a9de457
Tanner Saint <tsaint2004@gmail.com>
2024-04-16 23:14:42 +0000

    added documentation

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,18 +1,21 @@
 #include "hash_tables.h"
 
 /**
+ * returns a hash using the djb2 algorithm
+ * @str: string to be hashed
  *
+ * Return: hash
 */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-    int c;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);	
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);	
 }
