a32faf5b39cb6ff9958fbab5c0a4a02cb0d03ce9
Tanner Saint <tsaint2004@gmail.com>
2024-04-16 23:07:23 +0000

    made betty compliant

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,15 +9,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = (hash_table_t*)malloc(sizeof(hash_table_t));
 	unsigned int long i;
+
+	hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	hash_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t*));
+	hash_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 
 	if (hash_table->array == NULL)
 	{
