762a91fa3e4297dea25ec5359aa3833f49965748
Tanner Saint <tsaint2004@gmail.com>
2024-04-17 00:20:10 +0000

    added code to 3-hash_tables_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,49 @@
 #include "hash_tables.h"
 
 /**
+ * _strcmp- compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: different characters
+ */
+
+int _strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * *_strcpy- copies a string
+ * @dest: buffer to coppy string to
+ * @src: input string
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * hash_table_set- adds or updates an element of the hash table
  * @ht: pointer to the head node of the hash table
  * @key: key of the new node
@@ -11,16 +54,17 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new_node;
 	unsigned long int index;
+	hash_node_t *current;
 
 	if (key == NULL)
 	{
 		return (0);
 	}
 
-	index = key_index(key, ht->size);
-
-	hash_node_t *current = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 
 	while (current != NULL)
 	{
@@ -36,7 +80,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = current->next;
 	}
 
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		return (0);
@@ -57,5 +101,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
