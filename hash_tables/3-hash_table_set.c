ee46286444e88ceaa03603f325748f9acef6b7fe
Tanner Saint <tsaint2004@gmail.com>
2024-04-17 00:35:35 +0000

    made less than 40 lines

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -59,13 +59,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *current;
 
 	if (key == NULL || ht == NULL)
-	{
 		return (0);
-	}
-
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
-
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
@@ -73,28 +69,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(current->value);
 			current->value = strdup(value);
 			if (current->value == NULL)
-			{
 				return (0);
-			}
 			return (1);
 		}
 		current = current->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
@@ -102,9 +93,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
