3bcbaf2596a45b2ecad9f376e80cc379d5c91264
Tanner Saint <tsaint2004@gmail.com>
2024-04-17 00:51:49 +0000

    added code to 4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ *
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	current = NULL;
+
+	if (key == NULL || ht == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+
+	return (NULL);
+}
