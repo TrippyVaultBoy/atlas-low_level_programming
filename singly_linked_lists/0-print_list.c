b291b8502e5b0a77d65be475ad1a61a128b41305
Tanner Saint <tsaint2004@gmail.com>
2024-07-06 21:09:56 +0000

    added 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while(h)
+    {
+        if (h->next != NULL)
+        {
+            _putchar(h->len);
+            count++;
+            h = h->next;
+        }
+        else
+        {
+            _putchar(h->len);
+            count++;
+            return count;
+        }
+    }
+}
