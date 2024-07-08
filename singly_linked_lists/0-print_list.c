64991317f57437a10555fa1f4c1fa665c1b2bcee
Tanner Saint <tsaint2004@gmail.com>
2024-07-08 03:04:07 +0000

    added description

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_list - prints all elements of a list
+ * @h: head of list
+ * Return: number of nodes
+*/
+
 size_t print_list(const list_t *h)
 {
 	int count = 0;
