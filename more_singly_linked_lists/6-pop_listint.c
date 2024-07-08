6fcc4d3586109f96b0de6ab8f45741b9ee47495c
Tanner Saint <tsaint2004@gmail.com>
2024-07-08 22:20:08 +0000

    added 6-pop_listint.c

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a list
+ * @head: head of list
+ * Return: data of deleted node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data;
+
+	if (head == NULL || *head==NULL)
+		return (0);
+
+	data = (*head)->n;
+
+	temp = (*head)->next;
+	free(*head);
+	(*head) = temp;
+
+	return (data);
+}
