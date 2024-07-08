9304bda0f4a3710a3a32265413fdd7cdd2d27f13
Tanner Saint <tsaint2004@gmail.com>
2024-07-08 21:24:34 +0000

    added 4-free_listint.c

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head of list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while(temp)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
