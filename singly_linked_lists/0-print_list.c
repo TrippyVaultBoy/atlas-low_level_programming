073544fe54ecd7076438eb516d3ffb186bd7185c
Tanner Saint <tsaint2004@gmail.com>
2024-07-08 02:55:25 +0000

    fixed task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,14 +4,18 @@ size_t print_list(const list_t *h)
 {
     int count = 0;
 
-    while (h -> next != NULL)
+    while (h->next != NULL)
     {
         if (h->str == NULL)
         {
             printf("[0] (nil)\n");
         }
-        count++;
-        printf("{s}", h->str);
+        else
+        {
+            count++;
+            printf("[%d] %s\n", h->len, h->str);
+        }
+        h = h->next;
     }
     return (count);
 }
