5e9826c1bc37da6620e49ff1b27eb67486108c0a
Tanner Saint <tsaint2004@gmail.com>
2024-07-08 23:00:23 +0000

    added head NULL case

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (new_node == NULL || temp == NULL || *head == NULL)
+	if (new_node == NULL || temp == NULL || head == NULL)
 		return (NULL);
 
 	if (idx == 0)
