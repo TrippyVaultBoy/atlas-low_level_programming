8fcc498c116f08dd37e5c41dd139506e1d7ca1de
Tanner Saint <tsaint2004@gmail.com>
2024-07-09 00:46:46 +0000

    added 2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit value at an index
+ * @index: index of bit
+ * Return: value of bit at the given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	return ((n >> index) & 1);
+}
